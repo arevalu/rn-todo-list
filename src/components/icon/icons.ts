6b83f8c84f3bae0c2c56a4f2da7738fd29575b5c
luciana.arevalo <luciana.arevalo@ank.app>
2022-06-25 15:25:24 -0300

    chore: create icon component

---
+++ src/components/icon/icons.ts
@@ -0,0 +1,1 @@
+export { default as ArrowLeft } from '../../../assets/icons/ic_left_arrow.svg';
---
+++ src/components/icon/index.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable import/namespace */
+import React, { FunctionComponent } from 'react';
+import { Colors, ColorsKey } from '../../core/theme';
+import * as icons from './icons';
+
+/**
+ * Types
+ */
+
+export type IconName = keyof typeof icons;
+
+export interface IconsProps {
+  color: ColorsKey;
+  name: IconName;
+  size: number;
+  testID?: string;
+}
+
+/**
+ * Icons
+ */
+
+export const Icon: FunctionComponent<IconsProps> = ({
+  color,
+  name,
+  size,
+  testID,
+}) => {
+  const IconComponent = icons[name];
+
+  return (
+    <IconComponent
+      fill={Colors[color]}
+      height={size}
+      testID={testID}
+      width={size}
+    />
+  );
+};
