238ff66a19ad7f188e2923f7ab76f538fdd2588a
luciana.arevalo <luciana.arevalo@ank.app>
2022-06-28 13:07:22 -0300

    chore: android splash screen

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,27 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import {
   initialWindowMetrics,
   SafeAreaProvider,
 } from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
 import { RootNavigation } from '@core/routing/root-navigation';
 import { Colors } from '@core/theme';
 import ThemeProvider from '@core/theme/theme-provider';
 
-const App: FunctionComponent = () => (
-  <ThemeProvider>
-    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-      <StatusBar backgroundColor={Colors.white} barStyle="dark-content" />
-      <RootNavigation />
-    </SafeAreaProvider>
-  </ThemeProvider>
-);
+const App: FunctionComponent = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <ThemeProvider>
+      <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+        <StatusBar backgroundColor={Colors.white} barStyle="dark-content" />
+        <RootNavigation />
+      </SafeAreaProvider>
+    </ThemeProvider>
+  );
+};
 
 export default App;
